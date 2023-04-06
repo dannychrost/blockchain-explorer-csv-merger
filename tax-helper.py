967af8619aa7332095d97fb181cc472a378775a9
dannychrost <71196563+dannychrost@users.noreply.github.com>
2023-04-06 01:15:28 -0400

    Fixed blank lines bug in between rows

--- tax-helper.py
+++ tax-helper.py
@@ -5,12 +5,12 @@ from datetime import datetime
 # Open 1 CSV file in write mode
 with open(input("Enter the name of the first .csv file to read from: "), "r") as file1, \
     open(input("Enter the name of the second .csv file to read from: "), "r") as file2, \
-        open(input("Enter the name of the third .csv file to write to: "), "w") as file3:
+        open(input("Enter the name of the third .csv file to write to: "), 'w', newline='') as file3:
     # Create CSV reader and writer objects
     reader1 = csv.reader(file1)
     reader2 = csv.reader(file2)
     file3.truncate(0)
-    reader3 = csv.writer(file3)
+    writer1 = csv.writer(file3)
     # Declare a variable that will tell us the column position of DateTime
     # -1 means we don't know the position yet
     dateTimeColumn = -1
@@ -31,7 +31,7 @@ with open(input("Enter the name of the first .csv file to read from: "), "r") as
     f2MoveOn = True
     lineR1 = ""
     lineR2 = ""
-    reader3.writerow(firstLineR1)
+    writer1.writerow(firstLineR1)
     i = 0
     while(not endOfFile1 or not endOfFile2):
         if(not endOfFile1 and f1MoveOn):
@@ -41,7 +41,7 @@ with open(input("Enter the name of the first .csv file to read from: "), "r") as
             except StopIteration:
                 endOfFile1 = True
                 f1MoveOn = False
-                print("end of 1")
+                print(f'endOfFile1: {endOfFile1}')
         if(not endOfFile2 and f2MoveOn):
             try:
                 lineR2 = next(reader2)
@@ -49,7 +49,7 @@ with open(input("Enter the name of the first .csv file to read from: "), "r") as
             except StopIteration:
                 endOfFile2 = True
                 f2MoveOn = False
-                print("end of 2")
+                print(f'endOfFile2: {endOfFile2}')
         # Convert the dates into datetime objects
         date1 = ""
         date2 = ""
@@ -61,19 +61,18 @@ with open(input("Enter the name of the first .csv file to read from: "), "r") as
         print(f'endOfFile1: {endOfFile1}')
         print(f'endOfFile2: {endOfFile2}')
         if (not endOfFile1 and endOfFile2):
-            reader3.writerow(lineR1)
+            writer1.writerow(lineR1)
             f1MoveOn = True
         elif (endOfFile1 and not endOfFile2):
-            reader3.writerow(lineR2)
+            writer1.writerow(lineR2)
             f2MoveOn = True
-            print("howdy")
         elif (endOfFile1 and endOfFile2):
             break
         elif (date1 <= date2):
-            reader3.writerow(lineR1)
+            writer1.writerow(lineR1)
             f1MoveOn = True
         elif (date1 > date2):
-            reader3.writerow(lineR2)
+            writer1.writerow(lineR2)
             f2MoveOn = True
 
 
@@ -84,31 +83,3 @@ with open(input("Enter the name of the first .csv file to read from: "), "r") as
 # 'DateTime', 'From', 'To', 'ContractAddress', 'Value_IN(ETH)', 
 # 'Value_OUT(ETH)', 'CurrentValue @ $1868.21/Eth', 'TxnFee(ETH)', 
 # 'TxnFee(USD)', 'Historical $Price/Eth', 'Status', 'ErrCode', 'Method']
-
-
-    
-
-'''
-# Open the .csv files in read mode
-with open(file_name, 'r') as file:
-    # Create a CSV reader object
-    reader = csv.reader(file)
-    # Declare a variable that will tell us the column position of DateTime
-    # -1 means we don't know the position yet
-    dateTimeColumn = -1
-
-
-    # Loop through each row in the .csv file
-    for row in reader:
-        currentColumn = 0
-        # Access each column in the row by index
-        # For example, row[0] is the value in the first column
-        print(row)
-        for column in row:
-            if(column == "DateTime"):
-                dateTimeColumn =  currentColumn
-                currentColumn += 1
-            else:
-                currentColumn += 1
-        print(dateTimeColumn)
-'''
